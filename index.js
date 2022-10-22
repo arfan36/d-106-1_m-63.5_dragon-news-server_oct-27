15dc5f8e3c26935a712c22bbbcbc34e5a0a9a695
arfan36 <arfanur36@gmail.com>
2022-10-22 13:31:00 +0600

    61-3 Load news category data from server

--- index.js
+++ index.js
@@ -16,12 +16,6 @@ app.get('/news-categories', (req, res) => {
     res.send(categories);
 });
 
-app.get('/news/:id', (req, res) => {
-    const id = req.params.id;
-    const selectedNews = news.find(n => n._id === id);
-    res.send(selectedNews);
-});
-
 app.get('/category/:id', (req, res) => {
     const id = req.params.id;
     if (id === '08') {
@@ -30,6 +24,16 @@ app.get('/category/:id', (req, res) => {
         const category_news = news.filter(n => n.category_id === id);
         res.send(category_news);
     }
+});
+
+app.get('/news', (req, res) => {
+    res.send(news);
+});
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
 });
 
 app.listen(port, () => {
